15da7b53c04753e6710901a9c513fe95886402df
Matti Pedersen <mrmattipants@gmail.com>
2023-04-10 10:17:53 -0500

    Add files via upload

---
+++ PS-SFTA/Get-FTA_AdobeAcrobatPro.ps1
@@ -0,0 +1,19 @@
+
+Import-Module -Name "$($PSScriptRoot)\PS-SFTA.ps1" -Force
+
+Get-FTA .acrobatsecuritysettings
+Get-FTA .api
+Get-FTA .aaui
+Get-FTA .bpdx
+Get-FTA .fcdt
+Get-FTA .fdf
+Get-FTA .joboptions
+Get-FTA .pdf
+Get-FTA .pdfxml
+Get-FTA .pdx
+Get-FTA .rmf
+Get-FTA .secstore
+Get-FTA .sequ
+Get-FTA .xdp
+Get-FTA .xfdf
+Get-PTA ACROBAT
---
+++ PS-SFTA/Get-FTA_AdobeReader.ps1
@@ -0,0 +1,14 @@
+
+Import-Module -Name "$($PSScriptRoot)\PS-SFTA.ps1" -Force
+
+Get-FTA .acrobatsecuritysettings
+Get-FTA .api
+Get-FTA .fdf
+Get-FTA .pdf
+Get-FTA .pdfxml
+Get-FTA .pdx
+Get-FTA .rmf
+Get-FTA .secstore
+Get-FTA .xdp
+Get-FTA .xfdf
+Get-PTA ACROBAT
